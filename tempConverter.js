22528ceb2158b0cce2a670edb2c2e666e994730c
Ray Cooper <rcooper1337@gmail.com>
2018-07-05 12:49:25 -0500

    test

---
+++ tempConverter.js
@@ -0,0 +1,17 @@
+var cTemp = 100; // temperature in Celsius
+// being generous with parenthesis
+var hTemp = ((cTemp * 9) / 5) + 32; // way easier to read, but it also would have worked fine without
+
+document.write("Temperature in Celsius: " + cTemp + " degrees.</br>");
+document.write("Temperature in Fahrenheit: " + hTemp + " degrees.");
+
+const tempConversion = (degrees) => ((degrees * 9) / 5) + 32; // arrow function to do work on any value
+
+// grab user defined temperature
+const userTemp = prompt('Enter in degrees fahrenheit to convert to celsius:');
+console.log(tempConversion(userTemp));
+
+// print results to the DOM
+document.write(`<br/><br/>Temperature in Fahrenheit: ${userTemp}</br>
+                Temperature in Celsius: ${tempConversion(userTemp)}`);
+
---
+++ variables.js
@@ -0,0 +1,41 @@
+var productCount = 2;
+var subtotal = 14.98;
+var shipping = 2.75;
+var total = subtotal + shipping;
+console.log(total);
+console.log('test');
+
+subtotal = total - shipping;
+var salesTax = total * 0.15;
+var productPrice = subtotal / productCount;
+console.log(productPrice);
+
+// % calculates the remainder of a division
+var itemsPerBox = 12;
+var itemsToBeBoxed = 40;
+var itemsinLastBox = itemsToBeBoxed % itemsPerBox;
+console.log(`Items remaining in last box: ${itemsinLastBox}`); // template literal
+
+// Increment and decrement
+var a = 0;
+a++;
+console.log(a);
+--a;
+console.log(a);
+--a;
+console.log(a);
+++a;
+console.log(a);
+a = 6;
+--a;
+console.log(a);
+
+// + operator can concatenate values and strings
+var firstname = "John";
+var surname = "Doe";
+var fullname = firstname + " " + surname;
+alert(fullname);
+
+var name = "David";
+var age = 45;
+alert(name + age);
